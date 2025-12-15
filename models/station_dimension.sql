a99441b47abd983459fde535d4f983ed5a30ddc3
shiina753@gmail.com <shiina753@gmail.com>
2025-12-15 12:27:05 +0000

    stg_bike model creation and bike dataset correction

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -7,7 +7,7 @@ start_station_name as station_name,
 START_LAT as station_lat,
 START_LNG as start_station_lng
 
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 
 where RIDE_ID != '"rideid"'
 
