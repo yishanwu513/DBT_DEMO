84cca0b1c13282f3211e2bf95139dcd841656550
shiina753@gmail.com <shiina753@gmail.com>
2025-12-15 03:52:42 +0000

    station_dimension model creation

---
+++ models/station_dimension.sql
@@ -0,0 +1,19 @@
+WITH BIKE as (
+
+select
+distinct
+START_STATIO_ID AS station_id,
+start_station_name as station_name,
+START_LAT as station_lat,
+START_LNG as start_station_lng
+
+from {{ source('demo', 'bike') }}
+
+where RIDE_ID != '"rideid"'
+
+
+)
+
+select
+*
+from BIKE
