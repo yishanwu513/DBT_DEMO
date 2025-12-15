fa08ad9c96135ad40d9c9993dd34e5c7d70fcd61
shiina753@gmail.com <shiina753@gmail.com>
2025-12-15 01:29:54 +0000

    date.utils macro

---
+++ macros/date.utils.sql
@@ -0,0 +1,23 @@
+{%macro get_season(x)%}
+
+CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
+    THEN 'WINTER'
+    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
+    THEN 'SPRING'
+    WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
+    THEN 'SUMMER'
+    ELSE 'AUTUMN' 
+    END 
+
+{%endmacro%}
+
+
+{%macro day_type(x)%}
+
+CASE 
+WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat','Sun')
+THEN 'WEEKEND'
+ELSE 'BUSINESSDAY'
+END 
+
+{%endmacro%}
